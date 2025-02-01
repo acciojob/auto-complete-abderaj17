4285918938e81aa0024e3b8157dd9f012dbfa740
Adarsh Agrahari <agrahariadarsh68@gmail.com>
2025-02-01 14:36:17 +0530

    com

---
+++ src/components/Search.jsx
@@ -0,0 +1,41 @@
+  import React, { useState } from 'react'
+
+
+const fruits = ["apple", "cherry", "banana", "data", "elderberry", "fig"];
+const Search = () => {
+    const [searchText, setSearchText] = useState("");
+    const [filteredFruits, setFilteredFruits] = useState([]);
+
+    const handleChange = (e)=>{
+        const value = e.target.value;
+        setSearchText(value);
+
+
+    const filtered = fruits.filter((fruit)=>
+    fruit.toLowerCase().includes(value.toLowerCase())
+);
+setFilteredFruits(filtered);
+}
+
+
+  return (
+    <div>
+        <label htmlFor='search'><strong>Search: </strong>
+        <input type='text' onChange={(e)=> e.target.value}/>
+       </label>
+       {searchText && (
+        <ul>
+            {filteredFruits.length > 0 ? (
+                filteredFruits.map((fruit, index)=> <li key={index}>
+                    {fruit}
+                </li>)
+            ) : (
+                <li>No match found</li>
+            )}
+        </ul>
+       )}
+    </div>
+  );
+}
+
+export default Search;
